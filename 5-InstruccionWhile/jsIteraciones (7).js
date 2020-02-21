6d426fc7acf523cac58ef31baba45bd73d8e0142
alumno <alumno@utnfra.com.ar>
2020-02-21 12:20:23 -0300

    clase 6

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,6 +4,23 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
+	var numero;
+
+do {
+	 numero = parseInt(prompt("ingrese un numero"));
+
+ while (isNaN(numero)){
+	numero = parseInt(prompt("esto no es un numeor"));
+
+}	
+
+acumulador = acumulador + 1;
+contador   = contador   + 1;
+
+seguir = prompt("Quiere ingresar otro numero");
+}while (seguir == "s"); 
+	
+promedio = acumulador / contador;
 
 
 document.getElementById('suma').value=acumulador;
