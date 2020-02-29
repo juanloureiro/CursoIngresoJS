6e1592fd4a46c34b9640128dc59619b808f9de95
juanloureiro <60608665+juanloureiro@users.noreply.github.com>
2020-02-29 11:20:48 -0300

    Add files via upload

---
+++ ocho.js
@@ -0,0 +1,67 @@
+function mostrar()
+{
+ var nombre             ;  
+ var personas           ;
+ var contadorDeDias     ;
+ var formaDePagos       ;
+ var
+
+ do {
+     nombre = prompt("Ingrese un nombre: ");
+ 
+ while (!(isNaN(nombre))){  
+
+    nombre = prompt("Eso no es un nombre ingreselo nuevamente: ");
+
+ }
+
+ personas = parseInt(prompt("Ingrese la cantidad de acompañantes"));
+
+
+
+
+    
+ 
+
+   
+
+
+ }     
+ 
+/*b)Para la gestión de un hotel,
+ingresar los siguientes datos validados de una reserva
+nombre del huésped
+cantidad de personas 
+cantidad de dia de estadia 
+forma de pago(efectivo , tarjeta o QR)
+
+informar el huésped que trajo más personas en una sola reserva.
+la cantidad de personas que se quedaron más días
+la forma de pago más utilizada.
+el promedio de cantidad de días por reserva*/
+
+/*  
+var nombre
+var cantidadDePersonas
+var cantidadDeDias
+var formaDePago
+var respuesta
+
+respuesta = s;
+while(respuesta == "s"){
+    nombre= prompt("ingrese nombre");
+    while  (!isNan(nombre)){
+        nombre = prompt("ingrese nombre")
+    }
+    cantidadDePersonas= prompt("cantidad de personas");
+    parseInt(cantidadDePersonas)
+    while(isNaN(cantidadDePersonas) || cantidadDePersonas > 0){
+        cantidadDePersonas("cantidad de personas");
+        parseInt(cantidadDePersonas);
+    }
+
+    
+}
+
+
+*/ 
